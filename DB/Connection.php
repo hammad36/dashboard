26baaebc7adb1977eac0acd76ad4425cd73991c3
Mohammed Hammad <mohamedhammad923@gmail.com>
2024-08-20 12:37:48 +0300

    edit connection file

--- DB/Connection.php
+++ DB/Connection.php
@@ -9,7 +9,7 @@ class Connection
     private static $instance = null;
     private $conn;
 
-    private function __construct($hostname = 'localhost', $userName = 'hammad', $password = '', $database = 'dash')
+    private function __construct($hostname = '', $userName = '', $password = '', $database = '')
     {
         $this->hostname = $hostname;
         $this->userName = $userName;
@@ -23,7 +23,7 @@ class Connection
         }
     }
 
-    public static function getInstance($hostname = 'localhost', $userName = 'hammad', $password = '', $database = 'dash')
+    public static function getInstance($hostname = '', $userName = '', $password = '', $database = '')
     {
         if (self::$instance === null) {
             self::$instance = new Connection($hostname, $userName, $password, $database);
