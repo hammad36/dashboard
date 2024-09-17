c1b818e37e8fa54aefd39ff656cee39717839258
Mohammed Hammad <mohamedhammad923@gmail.com>
2024-09-17 17:08:32 +0300

    manage delete files in invoice and product

--- src/views/invoice/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "../../../DB/Connection.php";
-include "../../classes/shared/Delete.php";
-
-$handler = new Delete();
-$handler->deleteRecord('Invoice', 'inv_number', $_GET['inv_number'], 'ilist.php');
