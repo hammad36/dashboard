b95a77aae1e2fb499e6bec0358763ad9c66e9896
Mohammed Hammad <mohamedhammad923@gmail.com>
2024-10-30 17:26:21 +0300

    product ui

--- views/invoice/default.view.php
+++ views/invoice/default.view.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="../../assets/css/style.css">
+<link rel="stylesheet" href="../../public/css/style.css">
 
 <link rel="stylesheet" href="../../public/css/proudctStyles.css">
 <title>Dashboard</title>
--- views/product/edit.view.php
+++ views/product/edit.view.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="../../assets/css/style.css">
 <link rel="stylesheet" href="../../public/css/proudctStyles.css">
+<link rel="stylesheet" href="../../public/css/style.css">
 
 <title>Dashboard</title>
 </head>
