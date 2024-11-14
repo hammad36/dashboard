a7e3e32ca8735f6e23946d3fa06aeb23149a403a
Mohammed Hammad <mohamedhammad923@gmail.com>
2024-11-14 19:18:51 +0200

    enhance mvc files more

--- src/views/product/deleteProduct.php
+++
@@ -1,6 +0,0 @@
-<?php
-include "../../../DB/Connection.php";
-include "../../classes/shared/Delete.php";
-
-$handler = new Delete();
-$handler->deleteRecord('Product', 'pro_id', $_GET['id'], 'plist.php');
